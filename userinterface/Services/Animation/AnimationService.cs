4224a154672ed4072bf574754e26526312264242
Lex <lex.k.moulton@gmail.com>
2025-10-29 23:26:01 -0400

    Fix AnimationService.cs missing imports
    Add Avalonia.Animation and Avalonia.Styling imports

--- userinterface/Services/Animation/AnimationService.cs
+++ userinterface/Services/Animation/AnimationService.cs
@@ -1,6 +1,8 @@
+using Avalonia.Animation;
 using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Styling;
 using System;
 using System.Collections.Generic;
 using System.Linq;
