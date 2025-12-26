f495b76368afd5b0d4b272408cd170cfc3810a45
Lex <lex.k.moulton@gmail.com>
2025-12-25 23:50:55 -0500

    Add shared driver types and DriverMapper

---
+++ userspace-backend/Driver/DriverMapper.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using System.Linq;
+using userspace_backend.Driver.Types;
+using userspace_backend.Model;
+
+namespace userspace_backend.Driver
+{
+    /// <summary>
+    /// Shared mapping logic for converting domain models to driver types.
+    /// This class is platform-agnostic and produces shared driver types
+    /// that can be consumed by platform-specific driver implementations.
+    /// </summary>
+    public static class DriverMapper
+    {
+        /// <summary>
+        /// Maps a ProfileModel to a DriverProfile.
+        /// </summary>
+        public static DriverProfile MapProfile(ProfileModel model)
+        {
+            DriverAccelArgs accelArgs = model.Acceleration.MapToDriver();
+
+            return new DriverProfile
+            {
+                Name = model.Name.ModelValue,
+                OutputDPI = model.OutputDPI.ModelValue,
+                YXOutputDPIRatio = model.YXRatio.ModelValue,
+                LROutputDPIRatio = model.Hidden.LeftRightRatio.ModelValue,
+                UDOutputDPIRatio = model.Hidden.UpDownRatio.ModelValue,
+                Rotation = model.Hidden.RotationDegrees.ModelValue,
+                Snap = model.Hidden.AngleSnappingDegrees.ModelValue,
+                MaximumSpeed = model.Hidden.SpeedCap.ModelValue,
+                MinimumSpeed = 0,
+                ArgsX = accelArgs,
+                ArgsY = accelArgs,
+                DomainXY = new Vec2<double>(
+                    model.Acceleration.Anisotropy.DomainX.ModelValue,
+                    model.Acceleration.Anisotropy.DomainY.ModelValue),
+                RangeXY = new Vec2<double>(
+                    model.Acceleration.Anisotropy.RangeX.ModelValue,
+                    model.Acceleration.Anisotropy.RangeY.ModelValue),
+                SpeedArgs = new DriverSpeedArgs
+                {
+                    CombineMagnitudes = model.Acceleration.Anisotropy.CombineXYComponents.ModelValue,
+                    LPNorm = model.Acceleration.Anisotropy.LPNorm.ModelValue,
+                    OutputSmoothHalflife = model.Hidden.OutputSmoothingHalfLife.ModelValue,
+                    InputSmoothHalflife = model.Acceleration.Coalescion.InputSmoothingHalfLife.ModelValue,
+                    ScaleSmoothHalflife = model.Acceleration.Coalescion.ScaleSmoothingHalfLife.ModelValue,
+                }
+            };
+        }
+
+        /// <summary>
+        /// Maps a device model to driver device settings.
+        /// </summary>
+        public static DriverDeviceSettings MapDevice(IDeviceModel deviceModel, string profileName)
+        {
+            return new DriverDeviceSettings
+            {
+                Id = deviceModel.HardwareID.ModelValue,
+                Name = deviceModel.Name.ModelValue,
+                ProfileName = profileName,
+                Config = new DriverDeviceConfig
+                {
+                    Disable = deviceModel.Ignore.ModelValue,
+                    DPI = deviceModel.DPI.ModelValue,
+                    PollingRate = deviceModel.PollRate.ModelValue,
+                    PollTimeLock = false,
+                    SetExtraInfo = false,
+                    MaximumTime = 200,
+                    MinimumTime = 0.1,
+                }
+            };
+        }
+
+        /// <summary>
+        /// Maps devices in a device group to driver device settings.
+        /// </summary>
+        public static IEnumerable<DriverDeviceSettings> MapDevicesInGroup(
+            string deviceGroup,
+            string profileName,
+            IEnumerable<IDeviceModel> allDevices)
+        {
+            return allDevices
+                .Where(d => d.DeviceGroup.ModelValue.Equals(deviceGroup))
+                .Select(dm => MapDevice(dm, profileName));
+        }
+
+        /// <summary>
+        /// Maps all devices from a mapping to driver device settings.
+        /// </summary>
+        public static IEnumerable<DriverDeviceSettings> MapDevicesFromMapping(
+            MappingModel mapping,
+            IEnumerable<IDeviceModel> allDevices)
+        {
+            return mapping.IndividualMappings.SelectMany(
+                dg => MapDevicesInGroup(dg.DeviceGroup, dg.Profile.Name.ModelValue, allDevices));
+        }
+
+        /// <summary>
+        /// Maps all profiles from a mapping to driver profiles.
+        /// </summary>
+        public static IEnumerable<DriverProfile> MapProfilesFromMapping(MappingModel mapping)
+        {
+            return mapping.IndividualMappings
+                .Select(m => m.Profile)
+                .Distinct()
+                .OfType<ProfileModel>()
+                .Select(MapProfile);
+        }
+    }
+}
---
+++ userspace-backend/Driver/Types/DriverTypes.cs
@@ -0,0 +1,127 @@
+namespace userspace_backend.Driver.Types
+{
+    /// <summary>
+    /// Acceleration mode types matching the driver's supported formulas.
+    /// </summary>
+    public enum AccelMode
+    {
+        Classic,
+        Jump,
+        Natural,
+        Synchronous,
+        Power,
+        Lut,
+        NoAccel
+    }
+
+    /// <summary>
+    /// Cap application modes.
+    /// </summary>
+    public enum CapMode
+    {
+        InOut,
+        Input,
+        Output
+    }
+
+    /// <summary>
+    /// Generic 2D vector struct.
+    /// </summary>
+    public struct Vec2<T>
+    {
+        public T X;
+        public T Y;
+
+        public Vec2(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    /// <summary>
+    /// Acceleration formula parameters.
+    /// </summary>
+    public struct DriverAccelArgs
+    {
+        public const int MaxLutPoints = 128;
+        public const int LutRawDataCapacity = 513;
+
+        public AccelMode Mode;
+        public bool Gain;
+        public double InputOffset;
+        public double OutputOffset;
+        public double Acceleration;
+        public double DecayRate;
+        public double Gamma;
+        public double Motivity;
+        public double ExponentClassic;
+        public double Scale;
+        public double ExponentPower;
+        public double Limit;
+        public double SyncSpeed;
+        public double Smooth;
+        public Vec2<double> Cap;
+        public CapMode CapMode;
+        public int LutLength;
+        public float[] LutData;
+    }
+
+    /// <summary>
+    /// Input speed calculation parameters.
+    /// </summary>
+    public struct DriverSpeedArgs
+    {
+        public bool CombineMagnitudes;
+        public double LPNorm;
+        public double InputSmoothHalflife;
+        public double ScaleSmoothHalflife;
+        public double OutputSmoothHalflife;
+    }
+
+    /// <summary>
+    /// Complete acceleration profile configuration.
+    /// </summary>
+    public struct DriverProfile
+    {
+        public string Name;
+        public Vec2<double> DomainXY;
+        public Vec2<double> RangeXY;
+        public DriverAccelArgs ArgsX;
+        public DriverAccelArgs ArgsY;
+        public DriverSpeedArgs SpeedArgs;
+        public double OutputDPI;
+        public double YXOutputDPIRatio;
+        public double LROutputDPIRatio;
+        public double UDOutputDPIRatio;
+        public double Rotation;
+        public double Snap;
+        public double MinimumSpeed;
+        public double MaximumSpeed;
+    }
+
+    /// <summary>
+    /// Device-specific configuration.
+    /// </summary>
+    public struct DriverDeviceConfig
+    {
+        public bool Disable;
+        public bool SetExtraInfo;
+        public bool PollTimeLock;
+        public int DPI;
+        public int PollingRate;
+        public double MinimumTime;
+        public double MaximumTime;
+    }
+
+    /// <summary>
+    /// Device settings including profile assignment.
+    /// </summary>
+    public struct DriverDeviceSettings
+    {
+        public string Name;
+        public string ProfileName;
+        public string Id;
+        public DriverDeviceConfig Config;
+    }
+}
