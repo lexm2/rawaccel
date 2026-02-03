41598080e8d0b066b01fa1be805e8ff354992ef3
Lex <lex.k.moulton@gmail.com>
2026-02-02 19:23:14 -0500

    Remove Unused code

--- userinterface/Commands/RelayCommand.cs
+++ userinterface/Commands/RelayCommand.cs
@@ -58,44 +58,4 @@ namespace userinterface.Commands
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
-
-    public class AsyncRelayCommand : ICommand
-    {
-        private readonly Func<Task> executeAsync;
-        private readonly Func<bool>? canExecute;
-        private bool isExecuting;
-
-        public AsyncRelayCommand(Func<Task> executeAsync, Func<bool>? canExecute = null)
-        {
-            this.executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
-            this.canExecute = canExecute;
-        }
-
-        public bool CanExecute(object? parameter) => !isExecuting && (canExecute?.Invoke() ?? true);
-
-        public async void Execute(object? parameter)
-        {
-            if (!CanExecute(parameter)) return;
-
-            isExecuting = true;
-            RaiseCanExecuteChanged();
-
-            try
-            {
-                await executeAsync();
-            }
-            finally
-            {
-                isExecuting = false;
-                RaiseCanExecuteChanged();
-            }
-        }
-
-        public event EventHandler? CanExecuteChanged;
-
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-        }
-    }
 }
